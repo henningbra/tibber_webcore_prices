988f7478340b598e9878b2c85913e57df2442cd1
Henning Bragge <henning.bragge@gmail.com>
2021-03-22 19:33:19 +0100

    fixed

--- webcore/manager.py
+++ webcore/manager.py
@@ -34,7 +34,7 @@ class PriceManager:
         elif self.is_energy_price(high_prices=True, hours=3):
             level = "EXPENSIVE"
         else:
-            level = "MODERATE"
+            level = "MODERATE2"
         logging.info(f'Prices are now at {level} level')
         return level
 
